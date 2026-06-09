54462cef49472279a1388dc56f2a695a92fa20aa
Nguyễn Vân Thương <vanth312006gmail.com@MacBook-Air-cua-Nguyen-71.local>
2026-06-09 21:17:53 +0700

    Add login page

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',        // thay bằng user MySQL của bạn
-  password: '123456',        // thay bằng password MySQL của bạn
+  password: 'thuOng3126@',        // thay bằng password MySQL của bạn
   database: 'web_login_db',
   waitForConnections: true,
   connectionLimit: 10,
